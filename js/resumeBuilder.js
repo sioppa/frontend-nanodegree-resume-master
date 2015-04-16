4a6cefeab3e84430267d1453b9c942a43ab4c3bc
sioppa <Retina@Sis-MacBook-Pro.att.net>
2015-04-16 11:02:44 -0700

    added only work Function, need to write details

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -121,3 +121,22 @@ education.display = function() {
     }
 };
 education.display();
+
+function display() {
+    for (job in work.jobs){
+        $("workExperience").append)HTMLworkStart);
+
+        var formattedEmployer = HTMLworkEmployer.replace
+            ("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+        $(".work-entry:last").append(formattedEmployerTitle);
+
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+
+        var formattedDescription = HTMLworkDescription.replace("%data%",work[job].description);
+        $(".work-entry:last").append(formattedDescription);
+        }
+}
+work.display();
